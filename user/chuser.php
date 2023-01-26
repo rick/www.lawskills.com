b15006e9f2bbf1874151634f297b1e02090a44b3
Rick Bradley <rick@rickbradley.com>
2023-01-26 16:02:24 -0600

    Archival commit
    This site was last updated in 2013, content last updated in the early 2000's. We wish to keep this content
    online for historical reference but don't expect to update it again. Will see if this is a fit for GitHub
    Pages hosting.

---
+++ user/chuser.php
@@ -0,0 +1,146 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta name="description" content="LawSkills.com: Free access to Georgia case law and Georgia code statutes.">
+<title>Lawskills.com</title>
+<link rel="stylesheet" type="text/css" href="../css/style.css">
+
+<SCRIPT LANGUAGE="JavaScript" TYPE="text/javascript">
+     <!--
+ 
+     function launchCenter(url, name, height, width) {
+       var str = "height=" + height + ",innerHeight=" + height;
+       str += ",width=" + width + ",innerWidth=" + width;
+       if (window.screen) {
+         var ah = screen.availHeight - 30;
+         var aw = screen.availWidth - 10;
+ 
+         var xc = (aw - width) / 2;
+         var yc = (ah - height) / 2;
+ 
+         str += ",left=" + xc + ",screenX=" + xc;
+         str += ",top=" + yc + ",screenY=" + yc;
+       }
+       return window.open(url, name, str);
+     }
+ 
+     // -->
+     </SCRIPT>
+</head>
+
+<body>
+<table class="topheader">
+<tr>
+<td>
+<a href="../index.html"><img hspace="15" vspace="3" src="../images/lawskills_pay.png" border="0" alt="lawskills"></a><br>
+</td>
+<td colspan="4" align="right">
+<!-- Search Google -->
+<center>
+<FORM method=GET action="http://www.google.com/custom">
+<TABLE bgcolor=#FFFFFF cellspacing=0 border=0>
+<tr valign=top><td>
+<A HREF="http://www.google.com/search">
+<IMG SRC="http://www.google.com/logos/Logo_40wht.gif" border=0 ALT=Google align=middle></A>
+</td>
+<td>
+<INPUT TYPE=text name=q size=31 maxlength=255 value="">
+<INPUT type=submit name=sa VALUE="Google Search">
+<INPUT type=hidden name=cof VALUE="S:http://www.lawskills.com;AH:center;AWFID:1827bd9a01f53624;">
+<input type=hidden name=domains value="lawskills.com"><br><input type=radio
+name=sitesearch value=""> search the Web <input type=radio name=sitesearch
+value="lawskills.com" checked>search LawSkills.com </td></tr></table></form></center
+><!-- Search Google -->
+
+</td>
+</tr>
+</table>
+
+<div class="didyouknow"><center>Did you know you can download our <a href="http://www.rickbradley.com/lawskills.html">entire database for free?</a></center></div>
+<br>
+<hr size="1" width="100%" noshade>
+
+<table width="100%"><tr valign=top>
+
+<td class="bigtable">
+
+<form action="" method="post">
+<table class="middle">
+<tr><th colspan="2">Login</th></tr>
+<tr><td colspan="2">
+	<b>Supply your email and password to log in.</b>
+</td></tr>
+<tr><td>Email address:</td>
+<td><input type="text" name="auth_username" value="" size="30" maxlength="50"></td></tr>
+<tr><td valign="top">Password:</td>
+<td><input type="password" name="auth_password" value="" size="30" maxlength="30"></td></tr>
+<tr><td>Remember Me?</td><td><select name="auth_save_uid"><option value="1">Yes</option><option value="0">No</option></select></td></tr>
+<tr><td>&nbsp;</td><td><input type="submit" name="submit" value="Login">
+<a class="forgot" href="../forgot.php">Help me, I forgot my password!</a>
+</td></tr>
+</table>
+<input type="hidden" name="action" value="auth_login">
+</form>
+<br><form action="http://www.lawskills.com/register.php" method="post">
+<table class="middle">
+	<tr><th colspan="2">New Member Registration</th></tr>
+		<tr><td colspan="2" class="instruction"><p>Please supply your email
+	address and a password, which you will use to log in to our site.
+	Also, we need your name, picture is optional.
+		<tr><td >Email Address:</td>
+				<td align="left"><input type="text" name="username" value="" size="25" maxlength="70"><br>
+				</td>
+			</tr>
+			<tr><td >Password:</td>
+		<td align="left"><input type="password" name="password" value="" size="25" maxlength="25"><br>
+		</td>
+	</tr>
+	<tr><td >Retype Password:</td>
+		<td align="left"><input type="password" name="password2" value="" size="25" maxlength="25"><br>
+				</td>
+	</tr>
+			<tr><td >Remember Password:</td>
+				<td align="left"><select name="password_saved" ><option
+		value="1" selected>Yes</option>
+		<option value="0" >No</option>
+		</select>
+		</td>
+	</tr>
+		<tr><td >Name:</td>
+		<td align="left"><input type="text" name="name" value="" size="25" maxlength="50"><br>
+				</td>
+	</tr>
+	<tr><td >Picture Link:</td>
+		<td align="left"><input type="text" name="picture" value="" size="25" maxlength="100"><br>
+		</td>
+	</tr>
+		<tr><td colspan="2" align="center"><input type="submit" name="submit" value="Register">
+	<input type="hidden" name="action" value="auth_register">
+	<input type="hidden" name="editmode" value="new">
+		<input type="hidden" name="referent" value="/user/chuser.php">
+	</td></tr>
+</table>
+</form>
+</td>
+</tr></table>
+
+<br>
+<hr size="1" width="100%" noshade>                              
+<p align="center">
+<a href="../index.html">Home</a> -
+<a href="../tour">Tour</a> -
+<a href="../disclaimer">Disclaimer</a> -
+<a href="../privacy">Privacy</a> - 
+<a href="../contact">Contact Us</a><br>
+Copyright &copy; 2000,2002,2004 Lawskills.com
+</p>
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-3097892-3";
+urchinTracker();
+</script>
+</body>
+</html>
